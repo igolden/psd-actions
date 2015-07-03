0cb59c1594772e9fa8653e43cf500939623dd12b
Ian Golden <ian@iangolden.com>
2015-07-03 17:58:17 -0600

    Width Actions

---
+++ width-8-columns.jsx
@@ -0,0 +1,17 @@
+#target photoshop  
+function main(){  
+  if(!documents.length) return;  
+  var startRulerUnits = app.preferences.rulerUnits;  
+  app.preferences.rulerUnits = Units.PIXELS;  
+  var doc = activeDocument;  
+  var res= doc.resolution;  
+  var LB = activeDocument.activeLayer.bounds;  
+  var Width= LB[2].value - LB[0].value;  
+  var onePix = 100/Width;  
+  var newHeight = onePix * 100;
+  var currentHeight = doc.activeLayer.height;  
+  var newWidth = onePix * 770;  
+  doc.activeLayer.resize( newWidth , currentHeight, AnchorPosition.MIDDLECENTER);   
+  app.preferences.rulerUnits = startRulerUnits;  
+}  
+main(); 
