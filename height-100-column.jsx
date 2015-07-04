212a24d292645748ec50a51db166718fefcaddfa
Ian Golden <ian@iangolden.com>
2015-07-03 18:46:08 -0600

    Adds the height script

---
+++ height-100-column.jsx
@@ -0,0 +1,16 @@
+#target photoshop  
+function main(){  
+  if(!documents.length) return;  
+  var startRulerUnits = app.preferences.rulerUnits;  
+  app.preferences.rulerUnits = Units.PIXELS;  
+  var doc = activeDocument;  
+  var res= doc.resolution;  
+  var LB = activeDocument.activeLayer.bounds;  
+  var Width= LB[2].value - LB[0].value;  
+  var onePix = 100/Width;  
+  var newHeight = onePix * 100;
+  var currentWidth = doc.activeLayer.width;  
+  doc.activeLayer.resize( currentWidth , newHeight, AnchorPosition.MIDDLECENTER);   
+  app.preferences.rulerUnits = startRulerUnits;  
+}  
+main(); 
